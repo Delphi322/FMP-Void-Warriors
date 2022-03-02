088382089a43cde1e3541d5d35c0dfabf3b9ec0a
DelphiFeverpitch <rawlinson322@gmail.com>
2022-03-02 00:02:29 +0000

    Added things
    fewmore sprites and the scripts I already have made

---
+++ Assets/Scripts/BetaScripts/BattleSystem.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public Text[] healthTexts;
+    public Text[] nameTexts;
+
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public BattleState state;
+
+    public BattleHUD playerHUD;
+    public EnemyHUD enemyHUD;
+
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    void Update()
+    {
+        playerHUD.SetHUD(playerUnit,nameTexts[0],healthTexts[0]);
+    }
+
+    IEnumerator SetupBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO =  Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        GameObject.Find("defend").SetActive(false);
+        GameObject.Find("heal").SetActive(false);
+        GameObject.Find("jump").SetActive(false);
+        GameObject.Find("hurt").SetActive(false);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        playerTurn();
+
+    }
+
+    IEnumerator PlayerAttack()
+    {
+
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+
+        yield return new WaitForSeconds(2f);
+
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            EnemyTurn();
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        yield return new WaitForSeconds(2f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            playerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        
+    }
+    
+    void playerTurn()
+    {
+        GameObject.Find("defend").SetActive(true);
+    }
+
+    public void onAttackButton()
+    {
+        if (state == BattleState.PLAYERTURN)
+            StartCoroutine(PlayerAttack());
+        else
+            return;
+
+    }
+}
