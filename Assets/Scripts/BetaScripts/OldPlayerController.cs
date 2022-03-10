5581186fc2314cfcc6f6cd0de269a954bfb9abb5
DelphiFeverpitch <rawlinson322@gmail.com>
2022-03-10 23:07:26 +0000

    Overworld debug
    Set up overworld and added 8 directional movement with the player facing said directions too.

---
+++ Assets/Scripts/BetaScripts/OldPlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldPlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private float currentMoveSpeed;
+
+    private Animator anim;
+    private Rigidbody2D myRigidbody;
+
+    private bool playerMoving;
+    public Vector2 lastMove;
+    public float ClampMoveX;
+    private Vector2 moveInput;
+
+    private static bool playerExists;
+
+    private bool attacking;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        if(!playerExists)
+        {
+            playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        lastMove = new Vector2(0, -1f);
+    }
+
+    void Update()
+    {
+        playerMoving = false;
+        if (!attacking)
+        {
+            moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                anim.SetFloat("ClampMoveX", Input.GetAxisRaw("Horizontal"));
+            }
+            if (moveInput != Vector2.zero)
+            {
+                myRigidbody.velocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+                playerMoving = true;
+                lastMove = moveInput;
+            }
+            else
+            {
+                myRigidbody.velocity = Vector2.zero;
+            }
+        }
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
+}
