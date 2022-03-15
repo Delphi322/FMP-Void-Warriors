59a4fa896033d4f86000d58817c4d0c4745a2f1c
DelphiFeverpitch <rawlinson322@gmail.com>
2022-03-15 23:50:08 +0000

    Messy attempts at animations
    Lotta time spent doing a whole lotta nothing

--- Assets/Scripts/GridPlayerController.cs
+++ Assets/Scripts/GridPlayerController.cs
@@ -8,8 +8,22 @@ public class GridPlayerController : MonoBehaviour
     private Vector3 origPos, targetPos;
     private float timeToMove = 0.2f;
 
+    public Vector2 lastMove;
+    private Vector2 moveInput;
+
+    private Animator anima;
+
     void Update()
     {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        lastMove = moveInput;
+
+        if (lastMove.x >= 0)
+            transform.eulerAngles = new Vector2(0, 180);
+        else
+            transform.eulerAngles = Vector2.zero;
+
         if (Input.GetKey(KeyCode.UpArrow) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.up));
         if (Input.GetKey(KeyCode.DownArrow) && !isMoving)
@@ -18,11 +32,19 @@ public class GridPlayerController : MonoBehaviour
             StartCoroutine(MovePlayer(Vector3.left));
         if (Input.GetKey(KeyCode.RightArrow) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.right));
+            
+
+        anima.SetFloat("MoveX", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+        anima.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anima.SetBool("IsMoving", isMoving);
+        anima.SetFloat("LastMoveX", Mathf.Abs(lastMove.x));
+        anima.SetFloat("LastMoveY", lastMove.y);
     }
 
     private IEnumerator MovePlayer(Vector3 direction)
     {
         isMoving = true;
+
 
         float elapsedTime = 0;
 
