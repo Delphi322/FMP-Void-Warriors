b95eacdaec38ffbd2e5fdebf2d4f924c96a7069f
Delphi322 <rawlinson322@gmail.com>
2022-03-18 16:24:26 +0000

    tile palettes sorted
    auto tile palettes also working but aigis is tall and hitboxes are kinda funky

--- Assets/Scripts/GridPlayerController.cs
+++ Assets/Scripts/GridPlayerController.cs
@@ -15,14 +15,14 @@ public class GridPlayerController : MonoBehaviour
 
     void Update()
     {
-        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+       // moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        lastMove = moveInput;
+       // lastMove = moveInput;
 
-        if (lastMove.x >= 0)
+       /* if (lastMove.x >= 0)
             transform.eulerAngles = new Vector2(0, 180);
         else
-            transform.eulerAngles = Vector2.zero;
+            transform.eulerAngles = Vector2.zero;*/
 
         if (Input.GetKey(KeyCode.UpArrow) && !isMoving)
             StartCoroutine(MovePlayer(Vector3.up));
@@ -34,11 +34,11 @@ public class GridPlayerController : MonoBehaviour
             StartCoroutine(MovePlayer(Vector3.right));
             
 
-        anima.SetFloat("MoveX", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+        /*anima.SetFloat("MoveX", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
         anima.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
         anima.SetBool("IsMoving", isMoving);
         anima.SetFloat("LastMoveX", Mathf.Abs(lastMove.x));
-        anima.SetFloat("LastMoveY", lastMove.y);
+        anima.SetFloat("LastMoveY", lastMove.y);*/
     }
 
     private IEnumerator MovePlayer(Vector3 direction)
